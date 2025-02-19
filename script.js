5078f039c3e36d4fee31263e998c1c222badc28d
NBaker9262 <Noahmathmaster@gmail.com>
2025-02-19 08:36:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function loadPage() {
+    let url = document.getElementById("urlInput").value;
+    if (!url.startsWith("http")) {
+        url = "https://" + url;
+    }
+    document.getElementById("browserFrame").src = url;
+}
